46a3611bb7aad218304c588fd7404276a307acf9
Alexander <TheThirteenthLight@yandex.ru>
2023-12-27 15:53:30 +0300

    upd

--- projectX -dev/server.js
+++ projectX -dev/server.js
@@ -1,21 +1,21 @@
 // importing packages
 
-const express = require('express')
-const admin = require('firebase-admin')
-const bcrypt = require('bcrypt')
-const path = require('path')
+const express = require("express")
+const admin = require("firebase-admin")
+const bcrypt = require("bcrypt")
+const path = require("path")
 
 //firebase admin initialization
-let serviceAccount = require('./ecom-website-70525-firebase-adminsdk-nhkyq-60461e3f4e.json')
+let serviceAccount = require("./ecom-website-70525-firebase-adminsdk-nhkyq-60461e3f4e.json")
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 })
 
 let db = admin.firestore()
 
 // deqclare static path
-let staticPath = path.join(__dirname, 'public')
+let staticPath = path.join(__dirname, "public")
 
 //intializing express.js
 const app = express()
@@ -27,68 +27,68 @@ app.use(express.json())
 //routes//
 
 //home route
-app.get('/', (req, res) => {
-	res.sendFile(path.join(staticPath, 'index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"))
 })
 //signup route
-app.get('/signup', (req, res) => {
-	res.sendFile(path.join(staticPath, 'signup.html'))
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(staticPath, "signup.html"))
 })
 
-app.post('/signup', (req, res) => {
-	let { name, email, password, number, tac, notification } = req.body
+app.post("/signup", (req, res) => {
+  let {name, email, password, number, tac, notification} = req.body
 
-	//form validations
-	if (name.length < 3) {
-		return res.json({ alert: 'name must be 3 letters long' })
-	} else if (!email.value.length) {
-		return res.json({ alert: 'enter your email' })
-	} else if (password.value.length < 8) {
-		return res.json({ alert: 'password should be 8 letters long' })
-	} else if (!number.value.length) {
-		return res.json({ alert: 'enter your phone number' })
-	} else if (!Number(number.value) || number.value.length < 10) {
-		return { alert: 'invalid number, please enter valid one' }
-	} else if (!tac) {
-		return { alert: 'you must agree to our terms and conditions' }
-	}
-	//store user in db
-	db.collection('users')
-		.doc(email)
-		.get()
-		.then((user) => {
-			if (user.exists) {
-				return res.json({ alert: 'user already exists' })
-			} else {
-				//encrypt password before storing it
-				bcrypt.genSalt(10, (err, salt) => {
-					bcrypt.hash(password, salt, (err, hash) => {
-						req.body.password = hash
-						db.collection('users')
-							.doc(email)
-							.set(req.body)
-							.then((data) => {
-								res.json({
-									name: req.body.name,
-									email: req.body.email,
-									seller: req.body.seller,
-								})
-							})
-					})
-				})
-			}
-		})
+  //form validations
+  if (name.length < 3) {
+    return res.json({alert: "name must be 3 letters long"})
+  } else if (!email.value.length) {
+    return res.json({alert: "enter your email"})
+  } else if (password.value.length < 8) {
+    return res.json({alert: "password should be 8 letters long"})
+  } else if (!number.value.length) {
+    return res.json({alert: "enter your phone number"})
+  } else if (!Number(number.value) || number.value.length < 10) {
+    return {alert: "invalid number, please enter valid one"}
+  } else if (!tac) {
+    return {alert: "you must agree to our terms and conditions"}
+  }
+  //store user in db
+  db.collection("users")
+    .doc(email)
+    .get()
+    .then(user => {
+      if (user.exists) {
+        return res.json({alert: "user already exists"})
+      } else {
+        //encrypt password before storing it
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            req.body.password = hash
+            db.collection("users")
+              .doc(email)
+              .set(req.body)
+              .then(data => {
+                res.json({
+                  name: req.body.name,
+                  email: req.body.email,
+                  seller: req.body.seller,
+                })
+              })
+          })
+        })
+      }
+    })
 })
 
 //404 route
-app.get('/404', (req, res) => {
-	res.sendFile(path.join(staticPath, '404.html'))
+app.get("/404", (req, res) => {
+  res.sendFile(path.join(staticPath, "404.html"))
 })
 
 app.use((req, res) => {
-	res.redirect('/404')
+  res.redirect("/404")
 })
 
 app.listen(3000, () => {
-	console.log('listening on port 3000.......')
+  console.log("listening on port 3000.......")
 })
